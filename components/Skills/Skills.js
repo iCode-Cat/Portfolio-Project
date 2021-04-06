caa2f3ce170e7336cbd51b06c5a560f80945a7b6
iCode-Cat <devranboyac8@gmail.com>
2021-04-06 10:11:10 +0300

    Skills section built

---
+++ components/Skills/Skills.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import style from './Skills.module.scss';
+const Skills = () => {
+    const [experience, setExperience] = useState(
+        [
+            
+        {icon:'devicon-html5-plain',title:'HTML', caption:'caption'},
+        {icon:'devicon-css3-plain',title:'CSS', caption:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, aperiam? Eaque illo debitis architecto quibusdam! Esse aperiam voluptates quos placeat, dolorem expedita enim eos nisi, qui ipsa a officia magni.' },
+        {icon:'devicon-javascript-plain',title:'JAVASCRIPT', caption:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, aperiam? Eaque illo debitis architecto quibusdam! Esse aperiam voluptates quos placeat, dolorem expedita enim eos nisi, qui ipsa a officia magni.' },
+        {icon:'devicon-react-original',title:'REACT', caption:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, aperiam? Eaque illo debitis architecto quibusdam! Esse aperiam voluptates quos placeat, dolorem expedita enim eos nisi, qui ipsa a officia magni.' },
+        {icon:'devicon-nodejs-plain',title:'NODE.JS', caption:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, aperiam? Eaque illo debitis architecto quibusdam! Esse aperiam voluptates quos placeat, dolorem expedita enim eos nisi, qui ipsa a officia magni.' },
+        {icon:'devicon-express-original',title:'EXPRESS.JS', caption:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, aperiam? Eaque illo debitis architecto quibusdam! Esse aperiam voluptates quos placeat, dolorem expedita enim eos nisi, qui ipsa a officia magni.' },
+        {icon:'devicon-mongodb-plain',title:'MONGODB', caption:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, aperiam? Eaque illo debitis architecto quibusdam! Esse aperiam voluptates quos placeat, dolorem expedita enim eos nisi, qui ipsa a officia magni.' },
+    ]
+    );
+
+    
+    return <div className={style.container}>
+        <h1>Coding Skills</h1>
+        {experience.map(x => <div className={style.flex}>
+            <p><i className={x.icon}></i></p>
+            <p>{x.title}</p>
+            {/* <p>{x.caption}</p> */}
+        </div>)}
+    </div>
+}
+ 
+export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Header from '../components/Header/Header'
 import Hero from '../components/Hero/Hero'
 import Profile from '../components/Profile/Profile'
-import Experiences from '../components/Experiences/Experiences'
-import styles from '../styles/Home.module.css'
+import Skills from '../components/Skills/Skills'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -12,11 +12,15 @@ export default function Home() {
       <Head>
         <title>MY PORTFOLIO</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.10.1/devicon.min.css"></link>
+        <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
       </Head>
       <Header/>
       <Hero/>
+      <div className={styles.line}>
       <Profile/>
-      <Experiences/>
+      <Skills/>
+      </div>
 
     </div>
   )
